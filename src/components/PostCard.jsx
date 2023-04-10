67e4dbddbdd05376e2e600ab5e613d2c4ede9154
gabrielaliera <gliera2990@gmail.com>
2023-04-10 13:20:15 -0700

    Create template

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function PostCard() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="Card">
+      
+    </div>
+  )
+}
+
+export default PostCard
