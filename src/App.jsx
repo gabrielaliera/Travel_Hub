00819c61fdf2b005d2ae76d39bbdc5a31d1e9d3b
gabrielaliera <gliera2990@gmail.com>
2023-04-11 13:46:36 -0700

    Delete un-needed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import React,{ useState, useEffect } from 'react'
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-
-import ReadPosts from './pages/ReadPosts'
 import CreatePost from './pages/CreatePost'
 import Home from './pages/Home'
 import NavBar from './components/NavBar'
-import { Link } from 'react-router-dom'
-import { supabase } from './supabaseClient'
 import './App.css'
 
 function App() {
-
   
   return (
     <>
       <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/create" element={<CreatePost/>}/>
-        
-      </Routes>
-    
+        <Route path="/create" element={<CreatePost/>}/> 
+      </Routes>    
     </>
-   
   )
 }
 
