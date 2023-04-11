16a420d57fbe877292aa0927d390d81fe2ebefe8
gabrielaliera <gliera2990@gmail.com>
2023-04-11 15:24:14 -0700

    Import css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import "./NavBar.css"
 
 const NavBar = () => {
 
   return (
-    <div className="NavBar">
+    <nav>
       <h1>Travel Hub</h1>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to='/create'> Create Post</Link></li>
       </ul>
-    </div>
+    </nav>
   )
 }
 
