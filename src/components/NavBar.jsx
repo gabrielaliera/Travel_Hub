8ec9c0a162fd3a33e45aeea166588050fb10cfdd
gabrielaliera <gliera2990@gmail.com>
2023-04-11 13:41:09 -0700

    template for nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function NavBar() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="NavBar">
+      <h1>Nav</h1>
+    </div>
+  )
+}
+
+export default PostCard
