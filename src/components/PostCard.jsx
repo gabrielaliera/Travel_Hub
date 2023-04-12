fce05ffd6b2234f1ac22ef470509e5df6f2fea43
gabrielaliera <gliera2990@gmail.com>
2023-04-11 22:11:16 -0700

    add icon and fix return div

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import "./PostCard.css"
 
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+
 
 const PostCard = ({id, title, content, date, imageURL, upvote}) => {
 
@@ -8,9 +10,10 @@ const PostCard = ({id, title, content, date, imageURL, upvote}) => {
     <div className="post">
         <p>Posted by You - {date}</p>
         <h2>{title}</h2>
-        {content ? <h3>{content}</h3> : null}
-        {imageURL ? <img src={imageURL} alt={title}/> : null}
-        {upvote}
+        <div>
+         {upvote} <ThumbUpOutlinedIcon/>
+        </div>
+        
     </div>
   )
 }
