fb45f7c22ecad808f17800e9823369f9419a992b
gabrielaliera <gliera2990@gmail.com>
2023-04-18 22:38:34 -0700

    add upvote component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react'
 import "./PostCard.css"
+import {parseDate} from "../utils/parseDate" 
+import Upvote from "./UpVote"
 
-import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
 
+const PostCard = ({userID, post}) => {
+ console.log(post)
 
-const PostCard = ({id, title, content, date, imageURL, upvote}) => {
+ const [upvotes, setUpVotes] = useState(0);
+
 
   return (
     <div className="post">
-        <p>Posted by You - {date}</p>
-        <h2>{title}</h2>
-        <p>{content}</p>
-        <br/>
-        <div className="upvote">
-         <ThumbUpOutlinedIcon/> {upvote} votes 
-        </div>
-        
+        <p>Posted by {userID == post.userID ? "You" : `@${post.userID}`} | {parseDate(post.created_at)}</p>
+        <h2>{post.title}</h2>
+        <p>{post.content}</p>
+        <Upvote post={post}/>
     </div>
   )
 }
