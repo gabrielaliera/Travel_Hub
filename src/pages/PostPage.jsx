a15186d756fb8b3af40f6eef4042d64f0cef0885
gabrielaliera <gliera2990@gmail.com>
2023-04-25 15:43:33 -0700

    Add update and delete sections

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import Comments from '../components/Comments';
 import { supabase } from "../supabaseClient";
 import {parseDate} from "../utils/parseDate" 
@@ -9,7 +9,9 @@ import UpVote from "../components/UpVote"
 
 const PostPage =  ({userID}) => {
 
+    const navigate = useNavigate();
     const { id } = useParams();
+
     const [upvote, setUpVote] = useState(0);
     const [post, setPost] = useState({})
     const [loading, setLoading] = useState(false);
@@ -40,10 +42,26 @@ const PostPage =  ({userID}) => {
             <div className="post-page">
                 <p>Posted by {userID == post.userID ? "You" : `@${post.userID}`} | {parseDate(post.created_at)}</p>
                 <h2>{post.title}</h2>
+                
                 {post.content ? <h3>{post.content}</h3> : null}
                 {post.image_url ? <img src={post.image_url} alt={post.title}/> : null}
+                
                 <UpVote post={post}/>
+                
                 <Comments userID={userID} comments={post.comments} setPost={setPost} postID={post.id}/>
+
+                <div className="update-delete">
+                    {id == post.id && userID == post.userID ? (
+                        <div>
+                            <Link to={"/update/" + id}>
+                                <i className="material-icons">edit</i>
+                            </Link>
+                            <i className="material-icons" onClick={handleDelete}>
+                            delete
+                            </i>
+                        </div>
+                    ) : null}
+                </div>
             </div>
         )
     }
