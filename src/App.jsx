549ef483530270977a3068b3100915672b492eb8
gabrielaliera <gliera2990@gmail.com>
2023-04-25 15:41:37 -0700

    Add update page route and userID to navBar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CreatePost from './pages/CreatePost'
 import Home from './pages/Home'
 import PostPage from './pages/PostPage'
 import NavBar from './components/NavBar'
+import UpdatePage from './pages/UpdatePage'
 import { supabase } from "./supabaseClient";
 import './App.css'
 
@@ -15,13 +16,14 @@ function App() {
   
   return (
     <>
-      <NavBar search={search} setSearch={setSearch}/>
+      <NavBar userID={userID} search={search} setSearch={setSearch}/>
       <Routes>
         <Route path="/" element={<Home userID={userID} 
                                        orderBy={orderBy} 
                                        setOrderBy={setOrderBy}
                                        search={search}/>}/>
         <Route path="/create" element={<CreatePost userID={userID}/>}/>
+        <Route path="/update/:id" element={<UpdatePage />}/> 
         <Route path="/post/:id" element={<PostPage userID={userID}/>} />
       </Routes>    
     </>
